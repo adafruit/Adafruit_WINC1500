8ed60a49704809fd7bfbcebeb6e58fedd76c4d4b
Sandeep Mistry <s.mistry@arduino.cc>
2016-02-12 14:32:30 -0500

    Wrap modifications in #ifdef ARDUINO

--- src/driver/source/nmasic.c
+++ src/driver/source/nmasic.c
@@ -678,6 +678,7 @@ sint8 nmi_get_otp_mac_address(uint8 *pu8MacAddr,  uint8 * pu8IsValid)
 	ret = nm_read_reg_with_ret(rNMI_GP_REG_2, &u32RegValue);
 	if(ret != M2M_SUCCESS) goto _EXIT_ERR;
 
+#ifdef ARDUINO
 	if (u32RegValue) {
 		ret = nm_read_block(u32RegValue|0x30000,(uint8*)&strgp,sizeof(tstrGpRegs));
 		if(ret != M2M_SUCCESS) goto _EXIT_ERR;
@@ -687,6 +688,11 @@ sint8 nmi_get_otp_mac_address(uint8 *pu8MacAddr,  uint8 * pu8IsValid)
 		ret = nm_read_reg_with_ret(rNMI_GP_REG_0, &u32RegValue);
 		if(ret != M2M_SUCCESS) goto _EXIT_ERR;
 	}
+#else
+	ret = nm_read_block(u32RegValue|0x30000,(uint8*)&strgp,sizeof(tstrGpRegs));
+	if(ret != M2M_SUCCESS) goto _EXIT_ERR;
+	u32RegValue = strgp.u32Mac_efuse_mib;
+#endif
 
 	if(!EFUSED_MAC(u32RegValue)) {
 		M2M_DBG("Default MAC\n");
@@ -716,6 +722,7 @@ sint8 nmi_get_mac_address(uint8 *pu8MacAddr)
 	ret = nm_read_reg_with_ret(rNMI_GP_REG_2, &u32RegValue);
 	if(ret != M2M_SUCCESS) goto _EXIT_ERR;
 
+#ifdef ARDUINO
 	if (u32RegValue) {
 		ret = nm_read_block(u32RegValue|0x30000,(uint8*)&strgp,sizeof(tstrGpRegs));
 		if(ret != M2M_SUCCESS) goto _EXIT_ERR;
@@ -725,6 +732,11 @@ sint8 nmi_get_mac_address(uint8 *pu8MacAddr)
 		ret = nm_read_reg_with_ret(rNMI_GP_REG_0, &u32RegValue);
 		if(ret != M2M_SUCCESS) goto _EXIT_ERR;
 	}
+#else
+	ret = nm_read_block(u32RegValue|0x30000,(uint8*)&strgp,sizeof(tstrGpRegs));
+	if(ret != M2M_SUCCESS) goto _EXIT_ERR;
+	u32RegValue = strgp.u32Mac_efuse_mib;
+#endif
 
 	u32RegValue &=0x0000ffff;
 	ret = nm_read_block(u32RegValue|0x30000, mac, 6);
